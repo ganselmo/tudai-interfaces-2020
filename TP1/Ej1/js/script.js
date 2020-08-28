b26a9de968a8753827c1c8a8d1b8444ab39dfe8b
Geronimo <geronimoalejandroanselmo@gmail.com>
2020-08-28 15:21:35 -0300

    Ejercicio 1

--- TP1/Ej1/js/script.js
+++ TP1/Ej1/js/script.js
@@ -1,1 +1,20 @@
-console.log("gero")
+
+const columns = 10;
+const rows = 10;
+
+let matrix = [];
+
+for (let i = 0; i < columns; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < rows; j++) {
+        matrix[i][j] = Math.random()*100;
+        
+    }
+}
+
+console.table(matrix)
+
+function returnMax()
+{
+    
+}
