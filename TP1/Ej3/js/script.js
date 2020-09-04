1082ea6e3947101a7abddcb628a16e68fb4e9205
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2020-09-04 16:36:52 -0300

    Commit Entregable

--- TP1/Ej3/js/script.js
+++ TP1/Ej3/js/script.js
@@ -1,5 +1,19 @@
-const ctx = document.getElementById("mycanvas").getContext("2d");
+const imgdt = new ImageData(300,400);
 
-ctx.fillStyle="#FF0000";
+let i;
 
-ctx.fillRect(20,20,150,150);
+for (i = 0; i < imgdt.data.length; i += 4) {
+    imgdt.data[i+0] = 255;
+    imgdt.data[i+1] = 0;
+    imgdt.data[i+2] = 0;
+    imgdt.data[i+3] = 255;
+}
+
+const ctx = document.querySelector("#mycanvas").getContext("2d").putImageData(imgdt,10,10);
+
+// document.addEventListener('mousedown',function(event)
+// {
+//     console.log(event);
+//     document.getElementById("mycanvas").getContext("2d").putImageData(imgdt,event.clientX,event.clientY);
+// });
+
--- TP1/Ej4/js/script.js
+++ TP1/Ej4/js/script.js
@@ -1,5 +1,29 @@
-const ctx = document.getElementById("mycanvas").getContext("2d");
+const canvas = document.getElementById('mycanvas');
+const ctx = canvas.getContext('2d');
+const imageData = ctx.createImageData(100, 100);
 
-ctx.fillStyle="#FF0000";
+console.log(ctx);
+// Iterate through every pixel
+function setPixel(imageData,x,y,r,g,b)
+{
+    let index= (x+y*imageData.width)*4;
+    imageData.data[index] = r;  // R value
+    imageData.data[index + 1] = g;    // G value
+    imageData.data[index + 2] = b;  // B value
+    imageData.data[index + 3] = 255;  // A value
+}
+  
 
-ctx.fillRect(20,20,150,150);
+for(let i =0;i<imageData.height;i++)
+{
+    console.log(imageData.height)
+    
+    for(let y =0;y<imageData.width;y++)
+    {
+        setPixel(imageData,i,y,(i/imageData.width)*255,(i/imageData.width)*255,(i/imageData.width)*255);
+    }
+}
+// Draw image data to the canvas
+ctx.putImageData(imageData, 20, 20);
+
+
