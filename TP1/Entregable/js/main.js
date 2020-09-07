f469e3e7939b26b5321e72fbc1bb2c87842763a3
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2020-09-07 12:07:25 -0300

    Maquetacion y canvas funcionando correctamente

--- TP1/Entregable/js/main.js
+++ TP1/Entregable/js/main.js
@@ -1,6 +1,57 @@
 "use strict"
 
-document.addEventListener("DOMContentLoaded",function()
-{
-    console.log("test1");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const canvas = document.querySelector('#mycanvas');
+    const input = document.querySelector('#file');
+    let context = canvas.getContext('2d');
+    
+    context.fillStyle = "#FFFFFF";
+    context.fillRect(0,0,canvas.width,canvas.heigth)
+
+
+
+
+
+    input.onchange = e =>{
+        const file = e.target.files[0];
+        console.log(file);
+        
+
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file)
+
+        reader.onload = readerEvent=>
+        {
+            const content =readerEvent.target.result;
+            const image = new Image();
+            image.src = content;
+
+            image.onload = function()
+            {
+                const imageAspectRatio = (1.0 * this.height)/this.height;
+                const imageScaleWidth = canvas.width;
+                const imageScaleHeight = canvas.width * imageAspectRatio;
+
+                context.drawImage(this,0,0,imageScaleWidth,imageScaleHeight);
+                console.log(context)
+            }
+        }
+    }
+    // Iterate through every pixel
+    function setPixel(imageData, x, y, r, g, b) {
+        let index = (x + y * imageData.width) * 4;
+        imageData.data[index] = r;  // R value
+        imageData.data[index + 1] = g;    // G value
+        imageData.data[index + 2] = b;  // B value
+        imageData.data[index + 3] = 255;  // A value
+    }
+
+    // Draw image data to the canvas
+  
+
+
+
+
 })
