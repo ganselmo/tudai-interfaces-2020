bb1f60d8426d5c5f5487e887ce9ce38130145c07
Geronimo <geronimoalejandroanselmo@gmail.com>
2020-10-20 23:48:34 -0300

    Funcionando Todo

--- TP3/Entregable/js/comentary.js
+++ TP3/Entregable/js/comentary.js
@@ -57,4 +57,77 @@ document.addEventListener('DOMContentLoaded', function () {
 
     })
 
+    document.querySelector('.n-header .user input').addEventListener('focus',function()
+    {
+
+            this.style.width = '90%'
+
+        
+    })
+    document.querySelector('.n-header .user input').addEventListener('focusout',function()
+    {
+        if(this.value=='')
+        {
+            this.style.width = '40%'
+        }
+        else{
+            this.style.width = '90%'
+        }
+        
+    })
+
+    document.querySelector('.n-content textarea').addEventListener('focus',function()
+    {
+
+            this.style.width = '90%'
+
+        
+    })
+    document.querySelector('.n-content textarea').addEventListener('focusout',function()
+    {
+        if(this.value=='')
+        {
+            this.style.width = '40%'
+        }
+        else{
+            this.style.width = '90%'
+        }
+        
+    })
+
+    document.querySelector('.n-footer button').addEventListener('click',function(){
+
+        let newcomment = document.createElement('div');
+        newcomment.classList ='commentary'
+        newcomment.innerHTML = `
+        
+            <div class="comentary-box">
+                <div class="c-header">
+                    <div class="user">Geronimo Anselmo</div>
+                </div>
+                <div class="c-content">
+                    <span class="comment">
+                        "Lorem ipsum dolor sit amet consectetur adipiscing elit, lobortis purus lacus eleifend curabitur
+                        porttitor odio fames, tincidunt tortor turpis enim pharetra parturient. Imperdiet aenean quam
+                        "
+                    </span>
+                </div>
+                <div class="c-footer">
+                    <div class="rater">
+                        <div class="rate" data-rate="1"><img src="./assets/icons/starEmpty.svg" alt=""></div>
+                        <div class="rate" data-rate="2"><img src="./assets/icons/starEmpty.svg" alt=""></div>
+                        <div class="rate" data-rate="3"><img src="./assets/icons/starEmpty.svg" alt=""></div>
+                        <div class="rate" data-rate="4"><img src="./assets/icons/starEmpty.svg" alt=""></div>
+                        <div class="rate" data-rate="5"><img src="./assets/icons/starEmpty.svg" alt=""></div>
+                    </div>
+                </div>
+            </div>
+        
+        `
+       
+        document.querySelector('#commentarySection').appendChild(newcomment);
+
+        
+    })
+
 });
